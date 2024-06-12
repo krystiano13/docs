64cb8687404bc03a8d673d66f939f37369a14fe7
krystiano13 <crystianotv@gmail.com>
2024-06-12 15:59:39 +0200

    create model for Invites

--- server/app/models/document.rb
+++ server/app/models/document.rb
@@ -1,5 +1,8 @@
 class Document < ApplicationRecord
     validates :name, presence: true
     validates :user_id, presence: true
+
     belongs_to :user
+
+    has_many :invite
 end
--- server/app/models/invite.rb
+++ server/app/models/invite.rb
@@ -1,2 +1,7 @@
 class Invite < ApplicationRecord
+    validates :document_id, presence: true
+    validates :role, presence: true,
+    validates :accepted, presence: true
+
+    belongs_to :document
 end
