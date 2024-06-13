f5b4545d1346f979946ed1623de29f507808c46c
krystiano13 <crystianotv@gmail.com>
2024-06-13 15:58:39 +0200

    create home view intro animations

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -5,28 +5,61 @@ export function Home() {
   return (
     <div className="overflow-x-hidden w-[100vw] h-[100vh] flex flex-col-reverse md:flex-row justify-around items-center">
       <section className="w-3/5 h-full flex flex-col gap-8 items-center md:items-start justify-center p-8 md:p-12">
-        <h1 className="text-6xl md:text-8xl text-center md:text-left font-medium">
+        <motion.h1
+          animate={{
+            position: ["fixed", "static"],
+            x: [-500, 0],
+            scale: [0.5, 1],
+          }}
+          className="text-6xl md:text-8xl text-center md:text-left font-medium"
+        >
           Docs Online
-        </h1>
-        <p className="text-2xl md:text-3xl text-center md:text-left font-regular">
+        </motion.h1>
+        <motion.p
+          animate={{
+            position: ["fixed", "static"],
+            x: [-500, 0],
+            scale: [0.5, 1],
+          }}
+          transition={{
+            type: "spring",
+            bounce: 0.5,
+            delay: 0.15,
+          }}
+          className="text-2xl md:text-3xl text-center md:text-left font-regular"
+        >
           All your docs in one place
-        </p>
-        <button className="cursor-pointer p-2 pl-6 pr-6 text-xl text-white bg-violet-600 hover:bg-violet-500">
+        </motion.p>
+        <motion.button
+          animate={{
+            position: ["fixed", "static"],
+            x: [-500, 0],
+            scale: [0.5, 1],
+          }}
+          transition={{
+            type: "spring",
+            bounce: 0.5,
+            delay: 0.3,
+          }}
+          className="cursor-pointer p-2 pl-6 pr-6 text-xl text-white bg-violet-600 hover:bg-violet-500"
+        >
           Get Started
-        </button>
+        </motion.button>
       </section>
       <section className="w-2/5 h-full flex items-center justify-center">
         <motion.img
+          initial={{ scale: 0, x: 500 }}
           animate={{
             position: ["fixed", "static"],
             x: [500, 0],
-            scale: [0.5, 1],
+            scale: [0, 1],
           }}
           transition={{
             type: "spring",
             duration: 0.25,
             ease: "easeInOut",
             bounce: 0.25,
+            delay: 0.6,
           }}
           className="brightness-90"
           src={hero}
