cdc068b2fed51890c722b8368fe99af73eba9b8b
krystiano13 <crystianotv@gmail.com>
2024-06-13 14:41:30 +0200

    create styles for mobile version of home view

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -2,10 +2,14 @@ import hero from "../assets/images/hero.png";
 
 export function Home() {
   return (
-    <div className="w-[100vw] h-[100vh] flex justify-around items-center">
-      <section className="w-3/5 h-full flex flex-col gap-8 items-start justify-center p-12">
-        <h1 className="text-8xl font-medium">Docs Online</h1>
-        <p className="text-3xl font-regular">All your docs in one place</p>
+    <div className="w-[100vw] h-[100vh] flex flex-col-reverse md:flex-row justify-around items-center">
+      <section className="w-3/5 h-full flex flex-col gap-8 items-center md:items-start justify-center p-8 md:p-12">
+        <h1 className="text-6xl md:text-8xl text-center md:text-left font-medium">
+          Docs Online
+        </h1>
+        <p className="text-2xl md:text-3xl text-center md:text-left font-regular">
+          All your docs in one place
+        </p>
         <button className="cursor-pointer p-2 pl-6 pr-6 text-xl text-white bg-violet-600 hover:bg-violet-500">
           Get Started
         </button>
