1ff704550d95d3041239846c5f9a3e8031de4c50
krystiano13 <crystianotv@gmail.com>
2024-06-14 18:55:13 +0200

    create styles for login view

--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -1,17 +1,32 @@
 export function Login() {
   return (
-    <div className="w-[100vw] h-[100vh] flex flex-col gap-3 items-center justify-center">
-      <h2>Log In</h2>
-      <form>
-        <label htmlFor="email">Email Address</label>
-        <input type="text" placeholder="example@ex.com" required name="email" />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          placeholder="password"
-          required
-          name="password"
-        />
+    <div className="w-[100vw] h-[100vh] flex flex-col gap-5 items-center justify-center">
+      <h2 className="font-semibold text-3xl">Log In</h2>
+      <form className="p-8 w-[90vw] md:w-96 pt-12 pb-12 rounded-lg violet-shadow bg-violet-50 flex flex-col justify-start gap-6">
+        <div className="flex flex-col gap-3">
+          <label className="text-lg font-medium" htmlFor="email">
+            Email Address
+          </label>
+          <input
+            className="text-lg transition p-2 focus:border-b-violet-300 border-b-violet-400 border-b-2 border-b-solid outline-none"
+            type="text"
+            placeholder="example@ex.com"
+            required
+            name="email"
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <label className="text-lg font-medium" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="text-lg transition p-2 focus:border-b-violet-300 border-b-violet-400 border-b-2 border-b-solid outline-none"
+            type="password"
+            placeholder="password"
+            required
+            name="password"
+          />
+        </div>
       </form>
     </div>
   );
