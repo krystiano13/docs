59a7061e8574822401ca03984a3b71f551612fc4
krystiano13 <crystianotv@gmail.com>
2024-06-16 18:07:40 +0200

    finish login system

--- client/src/contexts/authContext.tsx
+++ client/src/contexts/authContext.tsx
@@ -2,21 +2,25 @@ import { createContext, useState } from "react";
 import type { ReactNode } from "react";
 
 interface Props {
-    children: ReactNode
+  children: ReactNode;
 }
 
 interface User {
-    id: number;
-    email: string;
+  id: number;
+  email: string;
+  token: string;
 }
 
-export const AuthContext = createContext({});
+export const AuthContext = createContext<{
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+}>({ user: { id: -1, email: "", token: "" }, setUser: () => {} });
 
 export const AuthContextProvider: React.FC<Props> = ({ children }) => {
-    const [user, setUser] = useState<User|null>(null);
-    return (
-        <AuthContext.Provider value={{ user, setUser }}>
-            { children }
-        </AuthContext.Provider>
-    )
-}
+  const [user, setUser] = useState<User | null>(null);
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -1,7 +1,48 @@
 import { motion } from "framer-motion";
 import { Form } from "../components/Form";
+import { useContext, useState } from "react";
+import { AuthContext } from "../contexts/authContext";
+import { useNavigate } from "react-router";
 
 export function Login() {
+  const userContext = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState<string[]>([]);
+
+  async function handleLogin(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    setErrors([]);
+
+    await fetch("http://127.0.0.1:3000/users/tokens/sign_in", {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.resource_owner) {
+          userContext.setUser({
+            id: data.resource_owner.id,
+            email: data.resource_owner.email,
+            token: data.token,
+          });
+
+          localStorage.setItem("refresh_token", data.refresh_token);
+
+          navigate("/");
+        } else {
+          if (data.error_description) {
+            const tmp_errors: string[] = [];
+            (data.error_description as string[]).forEach((item) => {
+              tmp_errors.push(item);
+            });
+
+            setErrors(tmp_errors);
+          }
+        }
+      });
+  }
+
   return (
     <div className="w-[100vw] h-[100vh] flex flex-col gap-8 items-center justify-center">
       <motion.h2
@@ -10,7 +51,16 @@ export function Login() {
       >
         Log In
       </motion.h2>
-      <Form mode="login" submit={(e) => e.preventDefault()} />
+      <Form mode="login" submit={handleLogin} />
+      <div id="errors">
+        {errors.map((item) => {
+          return (
+            <p className="text-red-500 text-center" key={item}>
+              {item}
+            </p>
+          );
+        })}
+      </div>
     </div>
   );
 }
