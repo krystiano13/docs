b443f89fa9c1d65f7ddeb7afd96cc2d04516e9b3
krystiano13 <crystianotv@gmail.com>
2024-06-17 20:01:33 +0200

    create option buttons for choose view

--- client/src/views/Choose.tsx
+++ client/src/views/Choose.tsx
@@ -1,7 +1,24 @@
-export function Choose() {
-    return (
-        <div className="w-[100vw] h-[100vh]">
+import { useState } from "react"
 
+export function Choose() {
+    const [option, setOption] = useState<"your"|"shared">("your");
+    return (
+        <div className="w-[100vw] h-[100vh] p-6">
+            <section className="pt-20 flex items-center" id="buttons">
+                <button 
+                    className={`${option == "your" && "text-white bg-violet-500"} font-medium text-lg p-2 pl-6 pr-6 border-violet-500 border-b-2 border-b-solid`} 
+                    onClick={() => setOption("your")}>
+                        Your Files
+                </button>
+                <button 
+                    className={`${option == "shared" && "text-white bg-violet-500"} font-medium text-lg p-2 pl-6 pr-6 border-violet-500 border-b-2 border-b-solid`}  
+                    onClick={() => setOption("shared")}>
+                        Shared Files
+                </button>
+            </section>
+            <section id="files" className="w-[100vw] h-auto overflow-y-auto">
+                <button>+</button>
+            </section>
         </div>
     )
 }
