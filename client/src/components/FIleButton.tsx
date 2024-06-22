b7b5145872c5b42702bd085fb7fe32e039ddff20
krystiano13 <crystianotv@gmail.com>
2024-06-22 17:52:41 +0200

    create styles for mobile version of choose view

--- client/src/components/FIleButton.tsx
+++ client/src/components/FIleButton.tsx
@@ -1,23 +1,33 @@
+import { motion } from "framer-motion";
+
 interface Props {
-    id: number;
-    title: string;
-    shared: boolean;
-    user?: string;
+  id: number;
+  title: string;
+  shared: boolean;
+  user?: string;
 }
 
-export const FileButton:React.FC<Props> = ({ id, title, shared, user }) => {
-    return (
-        <button
-            id={id.toString()} 
-            className="flex flex-col justify-center items-center rounded-lg btn-shadow transition-colors text-base font-semibold min-w-64 p-5 pl-8 pr-8"
-        >
-        {
-            title.length > 16 ? `${title.slice(0,15)} ...` : title
-        }
-        { shared && <br /> }
-        {   
-            shared && user ? "User: " : null
-        }
-        </button>
-    )
-}
+export const FileButton: React.FC<Props> = ({ id, title, shared, user }) => {
+  return (
+    <motion.button
+      initial={{ position: "fixed" }}
+      transition={{
+        type: "spring",
+        bounce: 0.3,
+        duration: 0.25,
+        delay: 0.15,
+      }}
+      animate={{
+        scale: [0.5, 1],
+        opacity: [0, 1],
+        position: ["fixed", "static"],
+      }}
+      id={id.toString()}
+      className="flex flex-col justify-center items-center rounded-lg btn-shadow transition-colors text-base font-semibold min-w-64 p-5 pl-8 pr-8"
+    >
+      {title.length > 16 ? `${title.slice(0, 15)} ...` : title}
+      {shared && <br />}
+      {shared && user ? "User: " : null}
+    </motion.button>
+  );
+};
--- client/src/views/Choose.tsx
+++ client/src/views/Choose.tsx
@@ -3,46 +3,62 @@ import { motion } from "framer-motion";
 import { FileButton } from "../components/FIleButton";
 
 export function Choose() {
-    const [option, setOption] = useState<"your"|"shared">("your");
-    return (
-        <div className="w-full h-full pt-6 p-0 md:p-6 flex flex-col">
-            <motion.section 
-                transition={{
-                    type: "spring",
-                    bounce: 0.3,
-                    duration: 0.25,
-                }}
-                animate={{ scale: [0.5, 1], opacity: [0, 1] }}
-                className="pt-20 justify-center md:justify-start flex items-center" id="buttons">
-                <button 
-                    className={`${option == "your" && "text-white bg-violet-500"} font-medium text-lg p-2 pl-6 pr-6 border-violet-500 border-b-2 border-b-solid`} 
-                    onClick={() => setOption("your")}>
-                        Your Files
-                </button>
-                <button 
-                    className={`${option == "shared" && "text-white bg-violet-500"} font-medium text-lg p-2 pl-6 pr-6 border-violet-500 border-b-2 border-b-solid`}  
-                    onClick={() => setOption("shared")}>
-                        Shared Files
-                </button>
-            </motion.section>
-            <motion.section 
-                transition={{
-                    type: "spring",
-                    bounce: 0.3,
-                    duration: 0.25,
-                    delay: 0.15,
-                }}
-                animate={{ scale: [0.5, 1], opacity: [0, 1] }}
-                id="files" 
-                className="pt-8 flex justify-center items-start gap-6 md:justify-start w-[100vw] min-h-80 h-auto overflow-y-auto"
-            >
-                <button 
-                    className="rounded-lg hover:bg-violet-400 transition-colors bg-violet-500 text-white text-xl font-bold min-w-64 p-5 pl-8 pr-8"
-                >
-                    +
-                </button>
-                <FileButton shared={true} user="Admin" id={1} title="Test" />
-            </motion.section>
-        </div>
-    )
+  const [option, setOption] = useState<"your" | "shared">("your");
+  return (
+    <div className="w-full h-full overflow-x-hidden pt-6 p-0 md:p-6 flex flex-col">
+      <motion.section
+        transition={{
+          type: "spring",
+          bounce: 0.3,
+          duration: 0.25,
+        }}
+        animate={{
+          scale: [0.5, 1],
+          opacity: [0, 1],
+          position: ["fixed", "static"],
+        }}
+        className="pt-20 justify-center md:justify-start flex items-center"
+        id="buttons"
+      >
+        <button
+          className={`${
+            option == "your" && "text-white bg-violet-500"
+          } font-medium text-lg p-2 pl-6 pr-6 border-violet-500 border-b-2 border-b-solid`}
+          onClick={() => setOption("your")}
+        >
+          Your Files
+        </button>
+        <button
+          className={`${
+            option == "shared" && "text-white bg-violet-500"
+          } font-medium text-lg p-2 pl-6 pr-6 border-violet-500 border-b-2 border-b-solid`}
+          onClick={() => setOption("shared")}
+        >
+          Shared Files
+        </button>
+      </motion.section>
+      <section
+        id="files"
+        className="pt-8 flex flex-col md:flex-row justify-center items-center md:items-start gap-6 md:justify-start w-[100vw] min-h-80 h-auto overflow-y-auto"
+      >
+        <motion.button
+          transition={{
+            type: "spring",
+            bounce: 0.3,
+            duration: 0.25,
+            delay: 0.15,
+          }}
+          animate={{
+            scale: [0.5, 1],
+            opacity: [0, 1],
+            position: ["fixed", "static"],
+          }}
+          className="rounded-lg hover:bg-violet-400 transition-colors bg-violet-500 text-white text-xl font-bold min-w-64 p-5 pl-8 pr-8"
+        >
+          +
+        </motion.button>
+        <FileButton shared={true} user="Admin" id={1} title="Test" />
+      </section>
+    </div>
+  );
 }
