790558e2231f3b2290b1e66bf303680c162edf85
krystiano13 <crystianotv@gmail.com>
2024-06-23 18:47:41 +0200

    create model for shares table

--- server/app/models/share.rb
+++ server/app/models/share.rb
@@ -1,2 +1,8 @@
 class Share < ApplicationRecord
+    belongs_to :user
+    belongs_to :document
+
+    validates :user_id, presence: true,
+    validates :document_id, presence: true,
+    validates :role, presence: true
 end
