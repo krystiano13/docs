03f7fa01c098f5306d329415f3797b391f1e820a
krystiano13 <crystianotv@gmail.com>
2024-06-23 18:56:55 +0200

    implement api function for getting shared documents from databaser

--- server/app/controllers/share_controller.rb
+++ server/app/controllers/share_controller.rb
@@ -1,10 +1,15 @@
 class ShareController < ApplicationController
     def index
         @shares = Share.where(user_id: params[:user_id])
+        @documents = Array.new
 
         if @shares.present?
+            @shares.each do |item|
+                @documents.push(item.document)
+            end
+
             return render json: {
-                :shares => @shares
+                :documents => @documents
             }, status: :ok
         else
             return render json: {
--- server/app/models/share.rb
+++ server/app/models/share.rb
@@ -2,7 +2,7 @@ class Share < ApplicationRecord
     belongs_to :user
     belongs_to :document
 
-    validates :user_id, presence: true,
-    validates :document_id, presence: true,
+    validates :user_id, presence: true
+    validates :document_id, presence: true
     validates :role, presence: true
 end
