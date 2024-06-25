fec6be21ea850cec37d119685c49855e4143456c
krystiano13 <crystianotv@gmail.com>
2024-06-25 16:22:11 +0200

    create entry animations for modal component

--- client/src/components/AddModal.tsx
+++ client/src/components/AddModal.tsx
@@ -3,14 +3,23 @@ import { motion } from "framer-motion";
 export function AddModal() {
   return (
     <motion.div
-      className="fixed w-full h-full flex justify-center items-center"
+      className="fixed bg-slate-950 bg-opacity-20 w-full h-full flex justify-center items-center"
       animate={{ opacity: [0, 1] }}
     >
-      <form>
+      <motion.form
+        transition={{
+          type: "spring",
+          bounce: 0.3,
+          duration: 0.25,
+          delay: 0.15,
+        }}
+        initial={{ scale: 0 }}
+        animate={{ scale: [0, 1] }}
+      >
         <input />
         <button type="submit">Create</button>
         <button>Cancel</button>
-      </form>
+      </motion.form>
     </motion.div>
   );
 }
