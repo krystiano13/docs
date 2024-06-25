eafa5a08fdb18a1d85fb532eb2d6e8593850b8d4
krystiano13 <crystianotv@gmail.com>
2024-06-25 16:30:18 +0200

    create styles for modal in 'Choose' view

--- client/src/components/AddModal.tsx
+++ client/src/components/AddModal.tsx
@@ -15,10 +15,22 @@ export function AddModal() {
         }}
         initial={{ scale: 0 }}
         animate={{ scale: [0, 1] }}
+        className="p-8 w-[90vw] md:w-96 pt-12 pb-12 rounded-lg violet-shadow bg-violet-50 flex flex-col justify-start gap-9"
       >
-        <input />
-        <button type="submit">Create</button>
-        <button>Cancel</button>
+        <input
+          type="text"
+          placeholder="Document Title"
+          className="text-lg transition p-2 focus:border-b-violet-300 border-b-violet-400 border-b-2 border-b-solid outline-none"
+        />
+        <button
+          className="text-white font-medium hover:bg-violet-400 transition-colors bg-violet-500 p-2 pl-6 pr-6 rounded-sm"
+          type="submit"
+        >
+          Create
+        </button>
+        <button className="text-white font-medium hover:bg-violet-400 transition-colors bg-violet-500 p-2 pl-6 pr-6 rounded-sm">
+          Cancel
+        </button>
       </motion.form>
     </motion.div>
   );
