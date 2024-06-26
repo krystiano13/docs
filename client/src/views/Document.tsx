e94b93329d7d0ad9743b64b70ec58e551b3e0842
krystiano13 <crystianotv@gmail.com>
2024-06-26 13:42:56 +0200

    create html structure for Document view

--- client/src/views/Document.tsx
+++ client/src/views/Document.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import { useState } from "react";
 
 export function Document() {
-    return (
-        <></>
-    )
+  const [invites, setInvites] = useState([]);
+  return (
+    <div className="overflow-x-hidden w-[100vw] h-[100vh] flex flex-col md:flex-row justify-around items-center">
+      <section id="invites" className="w-[90vw] md:w-2/5 max-h-[60vh]">
+        <div
+          id="invites_list"
+          className="bg-violet-50 w-full h-auto flex flex-col items-center"
+        >
+          <div>
+            <h3>InvitedUser@example.com</h3>
+            <button>Cancel</button>
+          </div>
+        </div>
+        <form>
+          <input type="email" required name="email" placeholder="User Email" />
+          <button type="submit">Invite</button>
+        </form>
+      </section>
+      <section id="options">
+        <button>Open Document</button>
+        <button>Rename Document</button>
+        <button>Delete Document</button>
+      </section>
+    </div>
+  );
 }
