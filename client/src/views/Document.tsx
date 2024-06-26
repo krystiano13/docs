78ce8bd6d3fa394ecb1b57ed261ebfb61d564ab5
krystiano13 <crystianotv@gmail.com>
2024-06-26 14:34:02 +0200

    create styles for 'Document' view

--- client/src/views/Document.tsx
+++ client/src/views/Document.tsx
@@ -3,26 +3,51 @@ import { useState } from "react";
 export function Document() {
   const [invites, setInvites] = useState([]);
   return (
-    <div className="overflow-x-hidden w-[100vw] h-[100vh] flex flex-col md:flex-row justify-around items-center">
-      <section id="invites" className="w-[90vw] md:w-2/5 max-h-[60vh]">
+    <div className="overflow-x-hidden w-[100vw] min-h-[100vh] flex flex-col md:flex-row justify-center gap-6 md:gap-0 md:justify-around items-center">
+      <section
+        id="invites"
+        className="w-[90vw] md:w-2/5 max-h-[50vh] flex flex-col gap-6"
+      >
         <div
           id="invites_list"
-          className="bg-violet-50 w-full h-auto flex flex-col items-center"
+          className="overflow-y-auto max-h-[50vh] bg-violet-50 w-full h-auto flex flex-col items-center rounded-lg violet-shadow gap-3 p-6"
         >
-          <div>
-            <h3>InvitedUser@example.com</h3>
-            <button>Cancel</button>
+          <div className="flex items-center gap-2 justify-between w-full">
+            <h3 className="md:text-lg font-medium">InvitedUser@example.com</h3>
+            <button className="text-white font-medium hover:bg-red-400 transition-colors bg-red-500 p-2 pl-6 pr-6 rounded-sm">
+              Cancel
+            </button>
           </div>
         </div>
-        <form>
-          <input type="email" required name="email" placeholder="User Email" />
-          <button type="submit">Invite</button>
+        <form className="p-8 w-full pt-12 pb-12 rounded-lg violet-shadow bg-violet-50 flex flex-col justify-start gap-6">
+          <input
+            className="text-lg transition p-2 focus:border-b-violet-300 border-b-violet-400 border-b-2 border-b-solid outline-none"
+            type="email"
+            required
+            name="email"
+            placeholder="User Email"
+          />
+          <button
+            className="w-full text-white font-medium hover:bg-violet-400 transition-colors bg-violet-500 p-2 pl-6 pr-6 rounded-sm"
+            type="submit"
+          >
+            Invite
+          </button>
         </form>
       </section>
-      <section id="options">
-        <button>Open Document</button>
-        <button>Rename Document</button>
-        <button>Delete Document</button>
+      <section
+        id="options"
+        className="bg-violet-50 violet-shadow flex flex-col gap-6 items-center justify-center rounded-lg p-6 pt-12 pb-12"
+      >
+        <button className="w-full text-white font-medium hover:bg-violet-400 transition-colors bg-violet-500 p-2 pl-6 pr-6 rounded-sm">
+          Open Document
+        </button>
+        <button className="w-full text-white font-medium hover:bg-violet-400 transition-colors bg-violet-500 p-2 pl-6 pr-6 rounded-sm">
+          Rename Document
+        </button>
+        <button className="w-full text-white font-medium hover:bg-violet-400 transition-colors bg-violet-500 p-2 pl-6 pr-6 rounded-sm">
+          Delete Document
+        </button>
       </section>
     </div>
   );
