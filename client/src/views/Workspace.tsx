4d91d995e5e8605fa91022592836c7fc8cafd29e
krystiano13 <crystianotv@gmail.com>
2024-06-28 16:34:16 +0200

    implement getting data from database and load it into workspace

--- client/src/views/Workspace.tsx
+++ client/src/views/Workspace.tsx
@@ -1,24 +1,34 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../contexts/authContext";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import debounce from "debounce-basic";
 
 export function Workspace() {
   const [value, setValue] = useState("");
   const [params, setParams] = useSearchParams();
 
   const navigate = useNavigate();
-
-  useEffect(() => {
-    console.log(value);
-  }, [value]);
+  const auth = useContext(AuthContext);
 
   useEffect(() => {
     if(!params.get("id")) {
       navigate("/choose");
     }
     else {
-      
+      fetch(`http://127.0.0.1:3000/api/documents/get_by_id/${params.get("id")}`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${auth.user?.token}`
+        }
+      })
+      .then(res => res.json())
+      .then(data => {
+        if(data.document.content !== "" && data.document.content) {
+          setValue(data.document.content);
+        }
+      })
     }
   }, []);
 
