c94064d710fa30ec95974a0f72e6c882ce781039
krystiano13 <crystianotv@gmail.com>
2024-06-28 11:17:03 +0200

    implement redirecting back when workspace page does not provide id param

--- client/src/views/Workspace.tsx
+++ client/src/views/Workspace.tsx
@@ -1,13 +1,26 @@
 import { useState, useEffect } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
 export function Workspace() {
   const [value, setValue] = useState("");
+  const [params, setParams] = useSearchParams();
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     console.log(value);
   }, [value]);
+
+  useEffect(() => {
+    if(!params.get("id")) {
+      navigate("/choose");
+    }
+    else {
+      
+    }
+  }, []);
 
   return (
     <div className="w-full h-full overflow-x-hidden pt-24 p-6 flex flex-col">
