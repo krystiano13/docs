39b3695a69d4e6dd8f9020bb4da2e5d48167efa2
krystiano13 <crystianotv@gmail.com>
2024-07-02 11:05:16 +0200

    remove sending user data with invite GET request

--- server/app/models/invite.rb
+++ server/app/models/invite.rb
@@ -4,6 +4,7 @@ class Invite < ApplicationRecord
     validates :document_id, presence: true
     validates :user_id, presence: true
     validates :role, presence: true
+    validates :owner_id, presence: true
 
     belongs_to :document
     belongs_to :user
